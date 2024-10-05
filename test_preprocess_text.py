a4f6eef71815be424c338c48a50e3dcb5dc2c31a
Wenhao.Dai <wenhao.dai633@gmail.com>
2024-10-05 15:15:50 +0800

    Add files via upload; first commit of this repo

---
+++ test_preprocess_text.py
@@ -0,0 +1,45 @@
+import re
+import os
+import hashlib
+import requests
+# from d2l import torch as d2l
+
+DATA_HUB = dict()
+DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+DATA_HUB['time_machine'] = (DATA_URL + 'timemachine.txt',
+                                '090b5e7e70c295757f55df93cb0a180b9691891a')
+
+def download(name, cache_dir=os.path.join('.', 'data')):  #@save
+    """下载一个DATA_HUB中的文件，返回本地文件名"""
+    assert name in DATA_HUB, f"{name} 不存在于 {DATA_HUB}"
+    url, sha1_hash = DATA_HUB[name]
+    os.makedirs(cache_dir, exist_ok=True)
+    fname = os.path.join(cache_dir, url.split('/')[-1])
+    if os.path.exists(fname):
+        sha1 = hashlib.sha1()
+        with open(fname, 'rb') as f:
+            while True:
+                data = f.read(1048576)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1.hexdigest() == sha1_hash:
+            return fname  # 命中缓存
+    print(f'正在从{url}下载{fname}...')
+    r = requests.get(url, stream=True, verify=True)
+    with open(fname, 'wb') as f:
+        f.write(r.content)
+    return fname
+
+def read_time_machine():  #@save
+    """将时间机器数据集加载到文本行的列表中"""
+    with open(download('time_machine'), 'r') as f:
+        lines = f.readlines()
+    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+
+
+if __name__ == '__main__':
+    lines = read_time_machine()
+    print(f'# 文本总行数: {len(lines)}')
+    print(lines[0])
+    print(lines[10])
